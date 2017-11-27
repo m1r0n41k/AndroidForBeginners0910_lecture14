e32d4245f8470c2c9bdcf61203b74d1cb8510986
Andrii Miroshnychenko <collosteam@gmail.com>
2017-11-27 20:56:50 +0200

    Added api call

--- app/src/main/java/com/drondon/androidforbeginners0910_lecture14/MainActivity.java
+++ app/src/main/java/com/drondon/androidforbeginners0910_lecture14/MainActivity.java
@@ -4,16 +4,23 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     RecyclerView recyclerView;
     Button updateButton;
     MyRecyclerViewAdapter adapter;
-    List<Coin> coinList;
+    List<Coin> coinList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +34,29 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         updateButton = findViewById(R.id.button);
+        updateButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Call<List<Coin>> call = API.get().getCoins(0, 20);
+        call.enqueue(new Callback<List<Coin>>() {
+            @Override
+            public void onResponse(Call<List<Coin>> call, Response<List<Coin>> response) {
+                if (response.isSuccessful()) {
+                    List<Coin> coins = response.body();
+                    coinList.clear();
+                    coinList.addAll(coins);
+                    adapter.notifyItemRangeInserted(0, coins.size());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Coin>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
